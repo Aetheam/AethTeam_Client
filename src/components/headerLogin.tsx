37022810e190147e5421d2e99319767c4a1d0791
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-05-19 03:04:08 +0200

    brouillons du header

---
+++ src/components/headerLogin.tsx
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { styled, alpha } from '@mui/material/styles';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
+import InputBase from '@mui/material/InputBase';
+import AethteamLogo from "../assets/AethTeamLogo.png"
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function ButtonAppBar() {
+  const [authButton, setAuthButton] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  React.useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthButton(true);
+    }
+  }, []);
+
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{backgroundColor: "#990909"}}>
+        <Toolbar  sx={{ marginRight: "10%", marginLeft: "10%", height: "2vh"}}>
+          <Typography  sx={{ flexGrow: 1 }}>
+            AethTeam Shop
+          </Typography>
+          
+          {authButton ? (
+              <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+            ) : (
+              <>
+                <Button color="inherit" sx={{padding: "1vh"}}>Login</Button>
+                <Button color="inherit" sx={{padding: "1vh"}} >register</Button>
+              </>
+            )
+
+          }
+        </Toolbar>
+      </AppBar>
+      <AppBar position="static" sx={{backgroundColor: "#312828"}}>
+        <Toolbar sx={{ marginRight: "10%", marginLeft: "10%"}}>
+          <Typography  sx={{ flexGrow: 1, display: "flex" }}>
+          <Box
+            component="img"
+            sx={{
+              height: 50,
+              width: 50,
+            
+            }}
+            alt="The house from the offer."
+            src={AethteamLogo}
+          />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          </Typography>
+         
+          
+          <Button color="inherit" sx={{padding: "1vh"}}>Textures</Button>
+          <Button color="inherit" sx={{padding: "1vh"}}>Plugins</Button>
+          <Button color="inherit" sx={{padding: "1vh"}} >Maps</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
